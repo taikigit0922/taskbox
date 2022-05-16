3001b83eb38a5d0297b99587d6b92c4b6a93be30
Taiki Nakanishi <t-nakanishi@h2-works.co.jp>
2022-05-16 19:34:01 +0900

    complete

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  //stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories:['../src/components/**/*.stories.js'],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
